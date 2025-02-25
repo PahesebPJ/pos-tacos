36d528f70399e36feb21df35649d9c9105b523e4
paheseb <pahesebw@gmail.com>
2025-02-25 14:45:35 -0700

    users module created

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,20 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TableModule } from './table/table.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    TableModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
+      port: 5431,
       username: 'postgres',
       password: '123456',
       database: 'postacos',
       autoLoadEntities: true,
       synchronize: true,
     }),
+    TableModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
