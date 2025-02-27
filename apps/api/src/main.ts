6564484896961de9933dbc596614fb51337da4b9
paheseb <pahesebw@gmail.com>
2025-02-26 19:52:57 -0700

    [release-api-products] Fixed static images bug

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as serveStatic from 'serve-static';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'images'));
+  app.use('/images', serveStatic(join(__dirname, '..', 'images')));
 
   await app.listen(process.env.PORT ?? 5000);
 }
