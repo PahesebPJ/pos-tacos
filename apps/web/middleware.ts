e560514dcc0af7b5e1b57c69a7fe0f00ee678a5b
paheseb <pahesebw@gmail.com>
2025-03-19 16:24:22 -0700

    Updated protected routes

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { decrypt } from "./app/lib/session";
 
-const protectedRoutes = ["/"];
+const protectedRoutes = ["/", "/products", "/accounting", "/inventory"];
 const publicRoutes = ["/login"];
 
 export default async function middleware(req: NextRequest) {
